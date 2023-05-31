73c284e1a1cc17e7994bff9e0f424aed0dcd3d7f
Marcus <marc303i@stud.kea.dk>
2023-05-31 17:56:07 +0200

    Tilføjede så at unpackPromise() i promise-handling.js kan returne promiset hvis der ikke er givet en metode til at håndtere den

--- js/util/promise-handling.js
+++ js/util/promise-handling.js
@@ -1,13 +1,18 @@
 
 
 function unpackPromise(promise, succesMethod, errorMethod) {
+    if (succesMethod === null || errorMethod === null) {
+        console.log("No successMethod or errorMethod was given, so will return the Promise.")
+        return promise;
+    }
+
     promise.then(data => {
-        console.log("Successfully unpacked a promise and received the Data:", data)
+        //console.log("Successfully unpacked a promise and received the Data:", data)
         //console.log("Will use the succesMethod(data):", succesMethod)
         succesMethod(data)
     }).catch(error => {
         console.error("Caught an error while unpacking a Promise: " + error.message)
-        console.log("Will use the errorMethod(error):", errorMethod)
+        //console.log("Will use the errorMethod(error):", errorMethod)
         errorMethod(error);
     })
 }
